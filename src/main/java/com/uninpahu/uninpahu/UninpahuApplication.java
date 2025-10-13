d61308ba9fe470807cd45798c92db86b008a8fa2
sanntr <sanntrv@gmail.com>
2025-10-12 20:26:08 -0500

    chore: initial commit

---
+++ src/main/java/com/uninpahu/uninpahu/UninpahuApplication.java
@@ -0,0 +1,14 @@
+package com.uninpahu.uninpahu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UninpahuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UninpahuApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/uninpahu/uninpahu/UninpahuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninpahu.uninpahu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UninpahuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
